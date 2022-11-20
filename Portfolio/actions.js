573757c8168120f2827eedc310be5de180acd98a
Ayla Hmadi <108673836+Ayla-hmadi@users.noreply.github.com>
2022-11-20 03:34:47 +0200

    Add files via upload
    Still need the footer

---
+++ Portfolio/actions.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector('#containerWr');
+let mouseDown = false;
+let startX, scrollLeft;
+
+slider.addEventListener('mousedown',(e) =>  {
+    mouseDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', ()=>{
+    mouseDown = false;
+    slider.classList.remove('active');
+
+});
+
+slider.addEventListener('mouseup',()=>{
+    mouseDown = false;
+    slider.classList.remove('active');
+
+});
+
+slider.addEventListener('mousemove',(e)=>{
+    if(!mouseDown) return;
+    e.preventDefault();
+
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x-startX; //if we want to fasten it, we multiply it by the ratio
+    slider.scrollLeft= scrollLeft- walk;
+});
